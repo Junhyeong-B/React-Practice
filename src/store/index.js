47565cbdc11b75b66731f29868060eb92fb622c3
JunHyeongB <aowlr0513@gmail.com>
2022-02-09 15:43:54 +0900

    feat: Redux Store에 Auth Slice 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "./auth";
 import { counterSlice } from "./counter";
 
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
