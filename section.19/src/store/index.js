a65ea559625b1c52e32f91e8783659f7b67e47b3
JunHyeongB <aowlr0513@gmail.com>
2022-02-09 17:39:12 +0900

    feat: Cart Store 추가

---
+++ section.19/src/store/index.js
@@ -0,0 +1,62 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const initialState = { cartItems: [] };
+
+const slice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      const { title, quantity, total, price } = action.payload;
+
+      state.cartItems.push({ title, quantity, total, price });
+    },
+    increaseItem(state, action) {
+      let index;
+      for (let i = 0; i < state.cartItems.length; i++) {
+        if (state.cartItems[i].title === action.payload) {
+          index = i;
+          break;
+        }
+      }
+      const { title, quantity, total, price } = state.cartItems[index];
+      state.cartItems[index] = {
+        title,
+        quantity: quantity + 1,
+        total: total + price,
+        price,
+      };
+    },
+    decreaseItem(state, action) {
+      let index;
+      for (let i = 0; i < state.cartItems.length; i++) {
+        if (state.cartItems[i].title === action.payload) {
+          index = i;
+          break;
+        }
+      }
+      const { title, quantity, total, price } = state.cartItems[index];
+
+      if (quantity === 1) {
+        state.cartItems = state.cartItems.filter(
+          (item) => item.title !== action.payload
+        );
+      } else {
+        state.cartItems[index] = {
+          title,
+          quantity: quantity - 1,
+          total: total - price,
+          price,
+        };
+      }
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: slice.reducer,
+});
+
+export const actions = slice.actions;
+
+export default store;
